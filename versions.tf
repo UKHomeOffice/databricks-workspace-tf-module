d5517ceb65ef7e1f1ef3cb6cbda4a712a5fd683a
DANIEL-ANDREWS-ho <daniel.andrews3@homeoffice.gov.uk>
2025-12-20 11:20:52 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,9 +1,32 @@
 terraform {
   required_providers {
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.84"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 6.24.0"
     }
   }
   required_version = ">= 1.5.0"
+}
+
+# Authenticate using environment variables: https://docs.aws.amazon.com/cli/v1/userguide/cli-configure-envvars.html
+# export AWS_ACCESS_KEY_ID=KEY_ID
+# export AWS_SECRET_ACCESS_KEY=SECRET_KEY
+# export AWS_SESSION_TOKEN=SESSION_TOKEN
+
+provider "aws" {
+  region = var.region
+}
+
+# Authenticate using environment variables: https://registry.terraform.io/providers/databricks/databricks/latest/docs#argument-reference
+# export DATABRICKS_CLIENT_ID=CLIENT_ID
+# export DATABRICKS_CLIENT_SECRET=CLIENT_SECRET
+
+provider "databricks" {
+  alias      = "mws"
+  host       = local.computed_databricks_provider_host
+  account_id = var.databricks_account_id
 }
