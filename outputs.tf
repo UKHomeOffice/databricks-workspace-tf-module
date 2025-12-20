90c5d93b46dec69989417716f54b7929e1f67364
DANIEL-ANDREWS-ho <daniel.andrews3@homeoffice.gov.uk>
2025-12-20 11:28:30 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "workspace_host" {
-  value = module.databricks_mws_workspace.workspace_url
+output "workspace_url" {
+  description = "Workspace URL."
+  value       = databricks_mws_workspaces.workspace.workspace_url
+}
+
+output "workspace_id" {
+  description = "Workspace ID."
+  value       = databricks_mws_workspaces.workspace.workspace_id
 }
