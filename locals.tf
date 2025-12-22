05a9a036225ce8438b1c4dd722504c49432a2ad2
DANIEL-ANDREWS-ho <daniel.andrews3@homeoffice.gov.uk>
2025-12-22 07:47:53 +0000

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,3 +1,17 @@
 locals {
   azs = data.aws_availability_zones.available.names
+
+  aws_partition = "aws"
+
+  databricks_provider_host = "https://accounts.cloud.databricks.com"
+
+  databricks_aws_account_id = "414351767826"
+
+  databricks_artifact_and_sample_data_account_id = "414351767826"
+
+  databricks_ec2_image_account_id = "601306020600"
+
+  assume_role_partition = "aws"
+
+  unity_catalog_iam_arn = "arn:aws:iam::414351767826:role/unity-catalog-prod-UCMasterRole-14S5ZJVKOTYTL"
 }
