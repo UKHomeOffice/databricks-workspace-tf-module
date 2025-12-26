283e9e5ee66ca9ac5cecfc168a2e9e2fc0c5ef8c
DANIEL-ANDREWS-ho <daniel.andrews3@homeoffice.gov.uk>
2025-12-26 08:36:23 +0000

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  azs = data.aws_availability_zones.available.names
+  azs            = data.aws_availability_zones.available.names
+  aws_account_id = data.aws_caller_identity.current.account_id
 
   aws_partition = "aws"
 
